e76cfab84212095d2012aed73d7daf5e57b7c6a7
Horlando <horlandoleao8@gmail.com>
2021-07-12 23:13:55 -0300

    settings connect db aws

--- models/Conexao.php
+++ models/Conexao.php
@@ -10,7 +10,8 @@ class Conexao
     public static function getInstance()
     {
         if (is_null(self::$conexao)) {
-            self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=agenda_contatos', 'root', '');
+            //self::$conexao = new \PDO('mysql:host=localhost;port=3306;dbname=agenda_contatos', 'root', '');
+            self::$conexao = new \PDO('mysql:host=db-dio-bootcamp.cgtbl7plyhbz.us-east-2.rds.amazonaws.com;port=3306;dbname=agenda_contatos', 'admin', 'leao1798');
             self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$conexao->exec('set names utf8');
         }
